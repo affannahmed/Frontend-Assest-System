2ad4c00c2f0e87e28190ab9b8d9e0c9d074cc8b5
Affan Ahmed <153582577+affannahmed@users.noreply.github.com>
2025-08-28 11:59:13 +0500

    Update config.js contabo link

--- Imagine_Frontend/Screens/config.js
+++ Imagine_Frontend/Screens/config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
   // BACKEND_URL: "http://75.119.143.9:9494/"
-  BACKEND_URL: "https://backend-asset-system.onrender.com"
+  BACKEND_URL: "https://portalassets.duckdns.org:8443"
   // BACKEND_URL: "http://127.0.0.1:5001"
 };
