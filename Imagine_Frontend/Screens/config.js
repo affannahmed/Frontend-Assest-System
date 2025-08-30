a1ff0a89b23e85843921d341fcaaf2d571854bb6
Affan Ahmed <153582577+affannahmed@users.noreply.github.com>
2025-08-30 16:26:44 +0500

    Update config.js set from http to https

--- Imagine_Frontend/Screens/config.js
+++ Imagine_Frontend/Screens/config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
   // BACKEND_URL: "http://75.119.143.9:9494/"
-  BACKEND_URL: "https://portalassets.duckdns.org:8443"
+  BACKEND_URL: "https://portalassets.duckdns.org"
   // BACKEND_URL: "http://127.0.0.1:5001"
 };
